74785f372de0e42fee2d424226f9d010a88e29d8
joey <joey.hoeppner@gmail.com>
2025-11-11 00:10:40 -0500

    feat: Slow down the animation

--- boards/shields/nice_oled/nice_epaper/widgets/animation.c
+++ boards/shields/nice_oled/nice_epaper/widgets/animation.c
@@ -59,7 +59,7 @@ void draw_animation(lv_obj_t *canvas, struct zmk_widget_screen *widget) {
     lv_obj_center(art);
 
     lv_animimg_set_src(art, (const void **)pokemon_imgs, 20);
-    lv_animimg_set_duration(art, CONFIG_NICE_OLED_GEM_ANIMATION_MS);
+    lv_animimg_set_duration(art, 1260);
     lv_animimg_set_repeat_count(art, LV_ANIM_REPEAT_INFINITE);
     lv_animimg_start(art);
 #else
