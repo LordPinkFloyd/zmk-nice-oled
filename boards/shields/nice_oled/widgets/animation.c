a10396d43cedbb4d290ceeb4c5800ad360e65704
joey <joey.hoeppner@gmail.com>
2025-11-10 22:53:50 -0500

    feat: Check out the pokemon animation

--- boards/shields/nice_oled/widgets/animation.c
+++ boards/shields/nice_oled/widgets/animation.c
@@ -78,15 +78,15 @@ void draw_animation(lv_obj_t *canvas, struct zmk_widget_screen *widget) {
     lv_obj_t *art2 = NULL;
 
 #if IS_ENABLED(CONFIG_NICE_OLED_GEM_ANIMATION)
-    art = lv_animimg_create(widget->obj);
-    lv_obj_center(art);
+//     art = lv_animimg_create(widget->obj);
+//     lv_obj_center(art);
 
-    lv_animimg_set_src(art, (const void **)crystal_imgs, 16);
-    lv_animimg_set_duration(art, CONFIG_NICE_OLED_GEM_ANIMATION_MS);
-    lv_animimg_set_repeat_count(art, LV_ANIM_REPEAT_INFINITE);
-    lv_animimg_start(art);
+//     lv_animimg_set_src(art, (const void **)crystal_imgs, 16);
+//     lv_animimg_set_duration(art, CONFIG_NICE_OLED_GEM_ANIMATION_MS);
+//     lv_animimg_set_repeat_count(art, LV_ANIM_REPEAT_INFINITE);
+//     lv_animimg_start(art);
 
-#elif IS_ENABLED(CONFIG_NICE_OLED_POKEMON_ANIMATION)
+// #elif IS_ENABLED(CONFIG_NICE_OLED_POKEMON_ANIMATION)
     /* If we have the Pokémon animation enabled */
     art = lv_animimg_create(widget->obj);
     lv_obj_center(art);
